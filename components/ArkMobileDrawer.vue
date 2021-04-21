a67f424541b06c727548306a2c28542144c20d6a
rzola-ie <ryan.zola@idea-evolver.com>
2021-04-21 19:32:33 -0400

    logout button

--- components/ArkMobileDrawer.vue
+++ components/ArkMobileDrawer.vue
@@ -60,21 +60,17 @@
           Settings</nuxt-link
         >
       </nav>
-      <button class="btn btn-blue btn-main" @click="logout">
+      <button class="btn btn-gray btn-main" @click="logout">
+        Logout
         <svg
+          class="inline-block w-5 h-5 ml-2 fill-current"
           xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          class="inline-block w-6 h-6 mr-2 stroke-current"
+          viewBox="0 0 20 20"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+            d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
           />
         </svg>
-        <span>Logout</span>
       </button>
     </div>
   </div>
