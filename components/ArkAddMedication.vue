4c3d0b0c991ef56eaa2667e815f4f7cd86bacd28
rzola-ie <ryan.zola@idea-evolver.com>
2021-04-23 17:01:18 -0400

    search and loading

--- components/ArkAddMedication.vue
+++ components/ArkAddMedication.vue
@@ -27,10 +27,10 @@
         <!-- search results -->
         <div
           v-else-if="searched && loading"
-          class="flex flex-col items-center justify-center w-full h-full text-3xl font-semibold text-center"
+          class="flex flex-col items-center justify-center w-full h-full text-3xl font-semibold text-center text-ie-gray-700"
         >
           <svg
-            class="w-24 h-24 mx-auto mb-4 text-ie-gray-900 animate-spin"
+            class="w-24 h-24 mx-auto mb-4 animate-spin"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
@@ -53,8 +53,42 @@
         </div>
         <div
           v-else-if="searched && searchResults.length === 0"
-          class="p-4 text-3xl font-bold text-center"
+          class="flex flex-col items-center justify-center w-full h-full text-3xl font-semibold text-center text-ie-gray-700"
         >
+          <svg fill="none" class="w-24 h-24 mx-auto mb-4">
+            <path
+              d="M36 28.024A18.05 18.05 0 0025.022 39M59.999 28.024A18.05 18.05 0 0170.975 39"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+            <ellipse
+              cx="37.5"
+              cy="43.5"
+              rx="4.5"
+              ry="7.5"
+              fill="currentColor"
+            ></ellipse>
+            <ellipse
+              cx="58.5"
+              cy="43.5"
+              rx="4.5"
+              ry="7.5"
+              fill="currentColor"
+            ></ellipse>
+            <path
+              d="M24.673 75.42a9.003 9.003 0 008.879 5.563m-8.88-5.562A8.973 8.973 0 0124 72c0-7.97 9-18 9-18s9 10.03 9 18a9 9 0 01-8.448 8.983m-8.88-5.562C16.919 68.817 12 58.983 12 48c0-19.882 16.118-36 36-36s36 16.118 36 36-16.118 36-36 36a35.877 35.877 0 01-14.448-3.017"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+            <path
+              d="M41.997 71.75A14.94 14.94 0 0148 70.5c2.399 0 4.658.56 6.661 1.556a3 3 0 003.999-4.066 12 12 0 00-10.662-6.49 11.955 11.955 0 00-7.974 3.032c1.11 2.37 1.917 4.876 1.972 7.217z"
+              fill="currentColor"
+            ></path>
+          </svg>
           lol what is that?
         </div>
         <!-- no results -->
