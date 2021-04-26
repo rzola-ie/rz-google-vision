5b0dbc9de7e3f9961973df92d402949d80e9a945
rzola-ie <ryan.zola@idea-evolver.com>
2021-04-26 17:00:52 -0400

    env variable

--- components/ArkAddMedication.vue
+++ components/ArkAddMedication.vue
@@ -10,7 +10,7 @@
       <h1 class="mb-4 font-serif text-xl text-center">Add Medication</h1>
       <div
         :class="`relative h-full ${
-          hasPhoto ? 'pt-0' : 'pt-16 '
+          hasPhoto ? 'pt-0' : 'pt-16'
         } overflow-y-scroll`"
       >
         <ark-medication-search
